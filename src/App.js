bbef9e6b9af371d4b30071276005573f5a87c0ad
essenlive <perchais.quentin@gmail.com>
2020-04-23 16:22:09 +0200

    🔥Reworked routing

--- src/App.js
+++ src/App.js
@@ -5,11 +5,12 @@ import Ask from './layouts/Ask.js';
 import Fab from './layouts/Fab.js';
 import Help from './layouts/Help.js';
 import About from './layouts/About.js';
+import Ecosystem from './layouts/Ecosystem.js';
 import Credits from './layouts/Credits.js';
 import { GiWorld } from 'react-icons/gi';
 import Models from './layouts/Models.js';
 import './App.css';
-import { BrowserRouter as Router, Link, Route} from "react-router-dom";
+import { BrowserRouter as Router, Link, Route, Switch} from "react-router-dom";
 
 
 const App = (props) => {
@@ -19,15 +20,34 @@ const App = (props) => {
     <div className={`${typeof (props.className) !== 'undefined' ? props.className : ''} Layout`}>
       <Menu className={'Layout__Menu'}/>
       <main className={'Layout__Content'}>
-          <Route path="/" exact component={Home} />
-          <Route path="/ask" exact component={Ask} />
-          <Route path="/fab" exact component={Fab} />
-          <Route path="/help" exact component={Help} />
-          <Route path="/about" exact component={About} />
-          <Route path="/credits" exact component={Credits} />
-          <Route path="/models" exact component={Models} />
+        <Switch>
+          <Route exact path="/">
+            <Home />
+            </Route>
+            <Route path="/ask">
+              <Ask/>
+            </Route>
+            <Route path="/fab">
+              <Fab/>
+            </Route>
+            <Route path="/help">
+              <Help />
+            </Route>
+            <Route path="/about">
+              <About />
+            </Route>
+            <Route path="/credits">
+              <Credits />
+            </Route>
+            <Route path="/ecosystem">
+              <Ecosystem />
+            </Route>
+            <Route path="/models">
+              <Models />
+            </Route>
+        </Switch>
       </main>
-        <Link className="Link__Ecosystem" to={'/models'}><GiWorld/></Link>
+        <Link className="Link__Ecosystem" to={'/ecosystem'}><GiWorld/></Link>
     </div>
     </Router>
 
